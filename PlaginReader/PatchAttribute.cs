e5fb7c7988713f2f4e9525426ad7dae9935198e1
Вадим Бежков <vadimbezhkov3112@gmail.com>
2021-03-01 13:28:08 +0300

    Добавьте файлы проекта.

---
+++ PlaginReader/PatchAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PlaginReader
+{
+    class PatchAttribute:Attribute
+    {
+        public static string Patch { get; set; }
+        public PatchAttribute(string pa)
+        {
+            Patch = pa;
+        }
+        public PatchAttribute()
+        {
+
+        }
+    }
+}
---
+++ PlaginReader/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+namespace PlaginReader
+{
+    [Patch(@"D:\C#\Homeworks\Plagin Reader\ClassLibrary1.dll") ]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirName = @"D:\C#\Homeworks\Plagin Reader";
+            string[] files = Directory.GetFiles(dirName);
+            foreach (var item in files)
+            {
+                Console.WriteLine(item);
+
+                Assembly asm = Assembly.LoadFile(item);
+                Console.WriteLine(asm.FullName);
+
+                Type type = asm.GetType("ClassLibrary1.Class1");
+
+                Type[] types = asm.GetTypes();
+
+                foreach (Type t in types)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(t.Name);
+                    Console.ResetColor();
+                }
+                GC.Collect();
+                object obj = Activator.CreateInstance(type);
+
+                MethodInfo method = type.GetMethod("GetBiography");
+                method.Invoke(obj, new object[] { });
+
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
